b34272e107165d03610d2b33700f01d58c9bc3fb
Zimlewis <z1ml3w1s123@gmail.com>
2026-04-25 20:06:15 +0700

    feat: add BasicFormatter

--- internal/formatter/basic.go
+++ internal/formatter/basic.go
@@ -6,14 +6,15 @@ import (
 	"github.com/zimlewis/tomato/internal/types"
 )
 
-type DefaultFormatter struct {}
+type BasicFormatter struct {}
 
-func (DefaultFormatter) Format(value types.CurrentResponse) (string, error) {
+func (BasicFormatter) Format(value types.CurrentResponse) (string, error) {
 	var clocks = []string{"pomodoro", "short", "long"}
 
 	minute := value.TimeLeft / 60
 	second := value.TimeLeft % 60
 	clock := clocks[value.Clock]
 
-	return fmt.Sprintf("Your %s session has %02d:%02d remaining", clock, minute, second), nil
+	return fmt.Sprintf("%s %02d %02d", clock, minute, second), nil
 }
+
--- internal/formatter/formatter.go
+++ internal/formatter/formatter.go
@@ -9,7 +9,21 @@ type Formatter interface {
 func NewFromString(s string) Formatter {
 	switch s {
 	case "waybar": return WaybarFormatter{}
-	case "default": return Default{}
+	case "basic": return BasicFormatter{}
+	case "default": return DefaultFormatter{}
 	}
-	return Default{}
+	return DefaultFormatter{}
 }
+
+// Basic structure of a formatter
+// type f struct {}
+//
+// func (f) Format(value types.CurrentResponse) (string, error) {
+// 	var clocks = []string{"pomodoro", "short", "long"}
+//
+// 	minute := value.TimeLeft / 60
+// 	second := value.TimeLeft % 60
+// 	clock := clocks[value.Clock]
+//
+// 	return fmt.Sprintf("%s %02d %02d", clock, minute, second), nil
+// }
